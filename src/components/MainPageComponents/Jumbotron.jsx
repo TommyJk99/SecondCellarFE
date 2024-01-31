e92f771b018efbdb7c27c5e7f71c4302cf1b9ba8
Thomas Mazzotta <thomasmazzotta99@gmail.com>
2024-01-31 10:57:08 +0100

    Adjustments for responiveness

--- src/components/MainPageComponents/Jumbotron.jsx
+++ src/components/MainPageComponents/Jumbotron.jsx
@@ -3,22 +3,22 @@ import WineBottle from "./WineBottle"
 
 export default function Jumbotron() {
   return (
-    <div className="relative md:ml-[8rem] lg:mt-20 lg:ml-40 xl:mt-20 xl:ml-40 font-bold font-poppins h-[380px] ">
+    <div className="relative sm:block mt-10 ml-[5rem] sm:mt-[3rem] sm:ml-[8rem] md:ml-[8rem] lg:mt-20 lg:ml-40 xl:mt-20 xl:ml-40 font-bold font-poppins sm:h-[380px] h-[240px]">
       {/* Zona del testo con z-index più alto */}
       <div className="absolute z-20 lg:top-0 lg:left-[-4rem]  xl:top-0 xl:left-0  drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)]">
-        <span className="md:text-[8rem] lg:text-[8rem] xl:text-[8rem] bold text-white">
-          Buy <span className="md:text-[4rem] lg:text-[4rem] xl:text-[4rem]">&</span>
+        <span className="text-[4rem] sm:text-[6rem] md:text-[8rem] lg:text-[8rem] xl:text-[8rem] bold text-white">
+          Buy <span className="text-[3rem] sm:text-[4rem] md:text-[5rem] ">&</span>
         </span>
 
-        <p className="md:text-[8rem] md:mt-[-50px] lg:text-[8rem] bold text-white xl:mt-[-60px] lg:mt-[-60px] xl:ml-10  md:ml-10">
+        <p className="text-[4rem] mt-[-30px] ml-8 sm:text-[6rem] md:text-[8rem] md:mt-[-50px] lg:text-[8rem] bold text-white xl:mt-[-60px] lg:mt-[-60px] xl:ml-10  md:ml-10 sm:ml-10 sm:mt-[-40px]">
           Sell <span className="lg:text-[6rem] md:text-[6rem] xl:text-[6rem] font-greatVibes">Wines</span>
         </p>
       </div>
 
       {/* Sfere con z-index inferiore */}
-      <div className="absolute hidden rounded-full lg:block lg:z-10 xl:z-10 xl:top-20 lg:top-20 bg-thema3 w-60 h-60 left-60 border-thema4 "></div>
+      <div className="absolute hidden rounded-full lg:block lg:z-10 xl:z-10 xl:top-20 lg:top-20 bg-thema1 w-60 h-60 left-60 border-thema4 "></div>
       <div className="absolute z-10 rounded-full md:top-8 md:left-96 lg:top-0 lg:left-0 w-36 h-36 bg-thema2 border-thema4 "></div>
-      <div className="absolute z-10 rounded-full w-52 h-52 top-16 md:right-16 lg:right-28 xl:right-56 bg-thema1 border-thema4 "></div>
+      <div className="absolute z-10 w-40 h-40 rounded-full sm:w-52 sm:h-52 top-16 right-14 sm:right-16 md:right-16 lg:right-28 xl:right-56 bg-thema1 border-thema4 "></div>
       <WineBottle />
     </div>
   )
--- src/components/MainPageComponents/WineWorldJumbo.jsx
+++ src/components/MainPageComponents/WineWorldJumbo.jsx
@@ -3,21 +3,21 @@ import WorldMap from "./Map"
 
 export default function WineWorldMain() {
   return (
-    <div className="relative mt-0 md:ml-28 lg:ml-40 font-bold font-poppins h-[380px]">
+    <div className="relative mt-0 ml-11 sm:ml-28 md:ml-28 lg:ml-40 font-bold font-poppins sm:h-[380px] h-[250px]">
       {/* Zona del testo con z-index più alto */}
       <div className="absolute top-0 lg:right-12 z-20 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,0.8)] ">
-        <span className="text-[8rem] bold text-white">
-          Explore <span className="text-[4rem]">the</span>
+        <span className="text-[4rem] sm:text-[6rem] md:text-[8rem] bold text-white">
+          Explore <span className="hidden sm:inline text-[4rem]">the</span>
         </span>
 
-        <p className="text-[8rem] bold text-white mt-[-60px] ml-10">
-          <span className="text-[6rem] font-greatVibes">Wine World</span>
+        <p className="text-[8rem] sm:text-[6rem] md:text-[8rem]  text-white mt-[-90px] sm:mt-[-60px] ml-4 sm:ml-10">
+          <span className=" text-[4rem] sm:text-[6rem] font-greatVibes">Wine World</span>
         </p>
       </div>
 
       {/* Sfere con z-index inferiore */}
-      <div className="absolute z-10 rounded-full bg-thema2 border-thema4 left-12 top-10 w-52 h-52 lg:left-32"></div>
-      <div className="absolute top-0 z-10 rounded-full w-80 h-80 right-56 bg-thema1 border-thema4"></div>
+      <div className="absolute top-0 z-10 rounded-full bg-thema2 border-thema4 left-12 sm:top-10 w-52 h-52 lg:left-32"></div>
+      <div className="absolute top-0 z-10 hidden rounded-full sm:block sm:w-64 sm:h-64 sm:right-32 md:right-56 bg-thema1 border-thema4"></div>
       <WorldMap />
     </div>
   )
